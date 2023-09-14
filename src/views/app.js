df5c065c64f8636d78f2e14c2bbd90fc2319bb15
RooT_Kim <bdd3452@gmail.com>
2023-09-14 22:10:34 +0900

    feat: header UI 제작 반응형 UI로 제작 - viewport width가 768px일때 변환

--- src/views/app.js
+++ src/views/app.js
@@ -30,42 +30,88 @@ class Component {
 export default class App extends Component {
   setup() {}
   template() {
-    return `<header class="navbar-wrapper">
-			<nav class="navbar">
-				${homeButton()}
-				${navigations()}
-				${searchBox()}
-				${infoNavigations()}
-				${drawerMenuButton()}
-			</nav>
-		</header>`;
+    return /* HTML */ `<header class="navbar-wrapper">
+      <nav class="navbar">
+        ${homeButton()} ${mainNavigations()} ${sideNavigations()}
+        ${navDrawerButton()}
+      </nav>
+      ${drawerModalBox()} ${searchModalBox()}
+    </header>`;
   }
-  setEvent() {}
+  setEvent() {
+    (() => {
+      const drawerModal = this.$target.querySelector('.nav-drawer-menu');
+      const drawerButton = this.$target.querySelector('.nav-drawer-button');
+      const searchModal = this.$target.querySelector('.navbar-search-modal');
+      const searchButton = this.$target.querySelector(
+        '.nav-side-item:nth-child(2)'
+      );
+      let drawerStatus = false;
+      let searchStatus = false;
+      const drawerChange = (status) => {
+        if (status) {
+          drawerModal.style.display = 'block';
+          drawerButton.innerHTML = icons.close;
+          drawerStatus = true;
+          return;
+        }
+        drawerModal.style.display = 'none';
+        drawerButton.innerHTML = icons.drawer;
+        drawerStatus = false;
+        return;
+      };
+      const searchChange = (status) => {
+        if (status) {
+          searchModal.style.display = 'flex';
+          searchButton.innerHTML = icons.underArrow;
+          searchStatus = true;
+          return;
+        }
+        searchModal.style.display = 'none';
+        searchButton.innerHTML = icons.search;
+        searchStatus = false;
+        return;
+      };
+
+      const drawerModalHandler = () => {
+        searchChange(false);
+        if (!drawerStatus) drawerChange(true);
+        else drawerChange(false);
+      };
+      const searchModalHandler = () => {
+        drawerChange(false);
+        if (!searchStatus) searchChange(true);
+        else searchChange(false);
+      };
+
+      const viewportResizeHandler = () => {
+        const viewportWidth = window.innerWidth;
+
+        if (viewportWidth <= 768) {
+          drawerButton.addEventListener('click', drawerModalHandler);
+          searchButton.addEventListener('click', searchModalHandler);
+        } else {
+          drawerButton.removeEventListener('click', drawerModalHandler);
+          searchButton.removeEventListener('click', searchModalHandler);
+          drawerChange(false);
+          searchChange(false);
+        }
+      };
+
+      drawerButton.addEventListener('click', drawerModalHandler);
+      searchButton.addEventListener('click', searchModalHandler);
+      window.addEventListener('resize', viewportResizeHandler);
+    })();
+  }
 }
 
-// drawerMenuButton 클릭 시 drawerMenu 화면에 출력
-const drawerMenu = () => {
-  const menu = `<div class="nav-drawer-menu"></div>`;
-  return menu;
-};
-
-const drawerMenuButton = () => {
-  return `<div class="nav-drawer">
-						<button class="nav-drawer-button">
-							${icons.drawer}
-						</button>
-					</div>`;
-};
-
 const homeButton = () => {
-  return `<div class="nav-home">
-						<a href="#" title="ground">
-							${icons.favicon}
-						</a>
-					</div>`;
+  return /* HTML */ `<div class="nav-home">
+    <a href="#" title="ground"> ${icons.favicon} </a>
+  </div>`;
 };
 
-const navigations = () => {
+const mainNavigations = () => {
   const categories = {
     favorite: ['인기', '/favorite'],
     subscribe: ['구독', '/subscribe'],
@@ -78,39 +124,80 @@ const navigations = () => {
     const menu = categories[category][0];
     const link = categories[category][1];
 
-    categoryList += `<li class="nav-item"><a href="${link}">${menu}</a></li>`;
+    categoryList += /* HTML */ `<li class="nav-main-item">
+      <a href="${link}">${menu}</a>
+    </li>`;
   }
 
-  return `<ul class="navbar-nav">
-					  ${categoryList}
-				  </ul>`;
+  return /* HTML */ `<ul class="navbar-nav">
+    ${categoryList}
+  </ul>`;
 };
 
-const searchBox = () => {
-  return `<div class="navbar-search">
-            <span class="navbar-search-icon">
-              ${icons.search}
-            </span>
-            <input type="text" placeholder="검색" />
-          </div>`;
-};
-
-const infoNavigations = () => {
-  const pages = {
-    mypage: ['회원가입', '/myPage'],
-    login: ['로그인', '/login'],
+const sideNavigations = () => {
+  const items = {
+    searchBox: navSearchBox(),
+    search: icons.search,
+    user: icons.user,
   };
 
-  let pageList = '';
+  let itemList = '';
 
-  for (const page in pages) {
-    const menu = pages[page][0];
-    const link = pages[page][1];
-
-    pageList += `<li class="nav-info-item"><a href="${link}">${menu}</a></li>`;
+  for (const item in items) {
+    itemList += /* HTML */ `<li class="nav-side-item flex-align-center">
+      ${items[item]}
+    </li>`;
   }
 
-  return `<ul class="navbar-nav-info">
-            ${pageList}
-	  			</ul>`;
+  return /* HTML */ `<ul>
+    ${itemList}
+  </ul>`;
+};
+
+const navSearchBox = () => {
+  return /* HTML */ `<div class="navbar-search flex-align-center">
+    <input type="text" placeholder="검색" />
+    <span class="navbar-search-icon flex-align-center"> ${icons.search} </span>
+  </div>`;
+};
+
+const searchModalBox = () => {
+  return /* HTML */ `<div class="navbar-search-modal">
+    <input type="text" placeholder="검색" />
+    <span class="navbar-search-icon flex-align-center"> ${icons.search} </span>
+  </div>`;
+};
+
+// navDrawerButton 클릭 시 drawerModalBox 화면에 출력
+const drawerModalBox = () => {
+  const categories = {
+    favorite: ['인기', '/favorite'],
+    subscribe: ['구독', '/subscribe'],
+    all: ['전체', '/all'],
+  };
+
+  let categoryList = /* HTML */ `<li class="nav-drawer-menu-icons">
+    <div class="nav-drawer-menu-icon flex-align-center">${icons.user}</div>
+  </li>`;
+
+  for (const category in categories) {
+    const menu = categories[category][0];
+    const link = categories[category][1];
+
+    categoryList += /* HTML */ `<li class="nav-drawer-menu-categories">
+      <a href="${link}">${menu}</a>
+    </li>`;
+  }
+
+  return /* HTML */ `<div class="nav-drawer-menu">
+    <ul>
+      ${categoryList}
+    </ul>
+  </div>`;
+};
+
+const navDrawerButton = () => {
+  return /* HTML */ `<div class="nav-drawer">
+    <button class="nav-drawer-button flex-align-center">${icons.drawer}</button>
+  </div>`;
 };
