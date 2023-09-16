b913e30ebbca8ae22c4040f48b0ee9109e199d41
RooT_Kim <bdd3452@gmail.com>
2023-09-17 01:19:10 +0900

    feat: 컴포넌트 분할 및 이벤트 버블링 처리 App에서 Header로 Component 분할 Component.js에 addEvent 메소드를 생성해서 이벤트 버블링 처리

--- src/views/components/common/Component.js
+++ src/views/components/common/Component.js
@@ -1,27 +1,52 @@
 export default class Component {
-	$target;
-	props;
-	state;
-	constructor($target, props) {
-		this.$target = $target;
-		this.props = props;
-		this.setup();
-		this.setEvent();
-		this.render();
-	}
-	setup() {}
-	mounted() {}
-	template() {
-		return ``;
-	}
-	render() {
-		this.$target.innerHTML = this.template();
-		this.mounted();
-	}
-	setEvent() {}
-	setState(newState) {
-		this.state = { ...this.state, ...newState };
-		this.render();
-	}
-	addEvent(eventType, selector, callback) {}
+  $target;
+  props;
+  state;
+
+  constructor($target, props) {
+    this.$target = $target;
+    this.props = props;
+    this.setup();
+    this.setEvent();
+    this.render();
+  }
+
+  setup() {}
+  mounted() {}
+  template() {
+    return ``;
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.mounted();
+  }
+
+  setEvent() {}
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.render();
+  }
+  addEvent(eventType, selector, callback) {
+    this.$target.addEventListener(eventType, (event) => {
+      if (!event.target.closest(selector)) return false;
+      callback(event);
+    });
+  }
+  removeEvent(eventType, selector, callback) {
+    this.$target.removeEventListener(eventType, (event) => {
+      if (!event.target.closest(selector)) return false;
+      callback(event);
+    });
+  }
+  addWindowEvent(eventType, callback) {
+    window.addEventListener(eventType, (event) => {
+      callback(event);
+    });
+  }
+  removeWindowEvent(eventType, callback) {
+    window.removeEventListener(eventType, (event) => {
+      callback(event);
+    });
+  }
 }
