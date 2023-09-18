92998979e7fd57ac5e51dd9e9b4f7b811974995e
RooT_Kim <bdd3452@gmail.com>
2023-09-18 21:08:26 +0900

    feat: contents component 기본 틀 생성

--- src/views/app.js
+++ src/views/app.js
@@ -1,13 +1,19 @@
 import Component from './components/common/Component.js';
 import Header from './components/header.js';
+import Content from './components/contents.js';
 
 export default class App extends Component {
   template() {
-    return /* HTML */ ` <div data-component="header"></div> `;
+    return /* HTML */ `
+      <div data-component="header"></div>
+      <div data-component="content"></div>
+    `;
   }
   mounted() {
     const $header = this.$target.querySelector('[data-component="header"]');
+    const $content = this.$target.querySelector('[data-component="content"]');
 
     new Header($header);
+    new Content($content);
   }
 }
