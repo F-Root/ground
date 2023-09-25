0f0b2fb55e94647bb4144a662e9b93b834f841ef
RooT_Kim <bdd3452@gmail.com>
2023-09-25 21:56:49 +0900

    feat: 회원가입(sign up) 기능 생성

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,24 @@
 import { Router } from 'express';
-// import { userService } from '../services/index.js';
+import { userService } from '../services/index.js';
 
 const userRouter = Router();
 
 userRouter.post('/signIn', async () => {});
-userRouter.post('/signUp', async () => {});
+userRouter.post('/signUp', async (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  try {
+    await userService.addUser({
+      email,
+      password,
+      name,
+    });
+
+    res.status(201).json({ sighUp: 'succeed' });
+  } catch (error) {
+    next(error);
+  }
+});
 userRouter.put('/update', async () => {});
 userRouter.delete('/', async () => {});
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,35 @@
 import { userModel } from '../db/index.js';
+import bcrypt from 'bcrypt';
 
 class UserService {
   constructor(userModel) {
     this.userModel = userModel;
   }
 
+  async addUser(userInfo) {
+    const { email, password, name } = userInfo;
+
+    const user = await this.userModel.findByEmail(email);
+    if (user) {
+      throw new Error('이미 사용 중인 이메일입니다.');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUserInfo = {
+      email,
+      password: hashedPassword,
+      name,
+    };
+
+    const createdNewUser = await this.userModel.create(newUserInfo);
+
+    if (!createdNewUser) {
+      throw new Error('회원가입에 실패하였습니다.');
+    }
+
+    return;
+  }
   async getUserToken() {}
 }
 
