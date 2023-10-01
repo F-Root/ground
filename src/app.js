939484f41313e00a6e1178c216d835a506e6318c
RooT_Kim <bdd3452@gmail.com>
2023-10-01 23:12:16 +0900

    feat: cookie-parser 추가, 로그인 기능

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import * as Routers from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// cookie
+app.use(cookieParser(process.env.COOKIE_SECRET));
+// Content-Type: application/json
 app.use(express.json());
+// Content-Type: application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 //view
