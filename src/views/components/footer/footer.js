a720c1eadeeb376de53df692c34f382e091fa980
RooT_Kim <bdd3452@gmail.com>
2025-04-29 16:06:56 +0900

    feat: header, content, footer 컴포넌트 관련 작업
    (header, content, footer): 컴포넌트 위치 재설정
    (header): 알림 수신 팝업 생성, 검색 기능 및 검색창 UI 생성

---
+++ src/views/components/footer/footer.js
@@ -0,0 +1,11 @@
+import Component from '../common/Component.js';
+
+export default class Footer extends Component {
+  template() {
+    return /* HTML */ `<div class="footer-wrapper">
+      <footer class="footer">
+        <div class="footer-copyright"><small>Copyright &copy; RooT</small></div>
+      </footer>
+    </div>`;
+  }
+}
