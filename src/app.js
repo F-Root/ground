4f58dc5724f5111daf273067c25a4b3812d85ae5
RooT_Kim <bdd3452@gmail.com>
2025-05-01 07:33:20 +0900

    feat(routing): 라우팅 변경 및 추가
     - 정적 라우팅은 webpack으로 빌드한 view 파일을 사용하도록 변경
    
     - 용도에 맞게 path 설정 & 라우팅(user, ground, content, comment, notificate
    
     - tokenCheck 미들웨어를 사용해 유저 credential을 갱신하는 엔드포인트 추가

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as Routers from './routers/index.js';
-import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
+import { errorHandler, tokenCheck } from './middlewares/index.js';
 
 const app = express();
 
@@ -12,12 +12,23 @@ app.use(express.json());
 // Content-Type: application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-//view
-app.use(Routers.viewsRouter);
+// view
+// app.use(Routers.viewsRouter);
+app.use(Routers.viewsRouterWebpack);
 
-//api routing
-app.use('/api', Routers.userRouter);
+// api routing
+app.use('/api/user', Routers.userRouter);
+app.use('/api/ground', Routers.groundRouter);
+app.use('/api/content', Routers.contentRouter);
+app.use('/api/comment', Routers.commentRouter);
+app.use('/api/notificate', Routers.notificateRouter);
 
+// renew credential
+app.post('/api/renew/credential', tokenCheck, (req, res, next) => {
+  res.status(200).json({ renew: 'succeed' });
+});
+
+// error handler
 app.use(errorHandler);
 
 export { app };
